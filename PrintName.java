2f26e2e7c03faadf87b1dade9024c4b543c77861
Your Name <saravanavel007s@gmail.com>
2026-05-27 20:19:36 +0530

    added to folder

---
+++ PrintName.java
@@ -0,0 +1,6 @@
+class PrintName{
+public static void main(String [] args)
+{
+System.out.println("saravana");
+}
+}
---
+++ Supermarket.java
@@ -0,0 +1,11 @@
+class Supermarket 
+{
+public static void main(String []args){
+	System.out.println("*****supermarket*****");
+	System.out.println("1 fruits 100");
+	System.out.println("2 milk 30");
+	System.out.println("3 ice 70");
+	System.out.print("**********");
+	
+}
+}
